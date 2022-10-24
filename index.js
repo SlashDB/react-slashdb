1549222935a1a88786e14266a75beccd2fc75159
jhslashdb <109535123+jhslashdb@users.noreply.github.com>
2022-10-24 11:01:43 -0400

    update
    removed old SDK imports

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import { default as auth } from './auth';
 
-//import { default as slashDB } from './slashdb';
-
 import {
   default as useSetUp,
   useDataDiscovery,
@@ -14,18 +12,12 @@ import {
   SlashDBConsumer,
 } from './Context';
 
-//import { default as setCookie, getCookie, delete_cookie } from './cookies';
-
 export {
   auth,
   SlashDBContext,
   SlashDBProvider,
   SlashDBConsumer,
-  //setCookie,
-  //getCookie,
-  //delete_cookie,
   useSetUp,
   useDataDiscovery,
-  useExecuteQuery,
-  //slashDB as default,
+  useExecuteQuery
 };
