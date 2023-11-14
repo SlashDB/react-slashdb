b10656ff5dab36d1238392230f4338ef5844fa98
Nelson Carrasquel <carrasquel@outlook.com>
2023-11-13 20:32:30 -0400

    auth method updated

--- src/auth.js
+++ src/auth.js
@@ -11,14 +11,59 @@ class Auth {
   /**
    * Log into a SlashDB instance with a username and password.  Executes provided function on successful login
    *
+   * @param {string} username user to log into SlashDB instance with
+   * @param {string} password password for user
    * @param {SlashDBClient} sdbClient a SlashDBClient object containing SlashDB host config, obtained from calling the useSetUp hook
    * @param {function} fnc function to be executed after successful login
    */
-  // async login(username, password, sdbClient, fnc) {
-    async login(sdbClient, fnc) {
+  async login(username, password, sdbClient, fnc) {
     try {
-      // await sdbClient.login(username, password)
-      await sdbClient.login()
+      await sdbClient.login(username, password)
+        .then( () => {
+          if (sdbClient.isAuthenticated()) {
+            fnc();
+          }
+        });
+    }
+    catch(e) {
+      console.error(e);
+      return;
+    }
+  }
+
+  /**
+   * Logs in to SlashDB instance. Only required when using SSO.
+   *
+   * @param {boolean} popUp - optional flag to sign in against the identity provider with a Pop Up window (false by default)
+   * @param {SlashDBClient} sdbClient a SlashDBClient object containing SlashDB host config, obtained from calling the useSetUp hook
+   * @param {function} fnc function to be executed after successful login
+   */
+  async loginSSO(popUp, sdbClient, fnc) {
+    try {
+      await sdbClient.loginSSO(popUp)
+        .then( () => {
+          if (sdbClient.isAuthenticated()) {
+            fnc();
+          }
+        });
+    }
+    catch(e) {
+      console.error(e);
+      return;
+    }
+  }
+
+  /**
+   * Logs in to SlashDB instance. Only required when using SSO.
+   *
+   * @param {Object} sso - optional settings to login with Single Sign-On
+   * @param {string} sso.idpId - optional identity provider id configured in SlashDB
+   * @param {string} sso.redirectUri - optional redirect uri to redirect browser after sign in
+   * @param {boolean} sso.popUp - optional flag to sign in against the identity provider with a Pop Up window (false by default)
+   */
+  async updateSSO(sso, sdbClient, fnc) {
+    try {
+      await sdbClient.updateSSO(sso)
         .then( () => {
           if (sdbClient.isAuthenticated()) {
             fnc();
