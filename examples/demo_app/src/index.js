8a75f74b87efaadd1c79e4fb08a1ee891bf7a506
Nelson Carrasquel <carrasquel@outlook.com>
2023-12-12 15:58:03 -0400

    sdk version reverted

--- examples/demo_app/src/index.js
+++ examples/demo_app/src/index.js
@@ -5,10 +5,6 @@ import App from './App';
 
 import { SlashDBProvider } from '@slashdb/react-slashdb';
 
-const host = "http://host.docker.internal:8000";	// set SlashDB host here
-const username = "test";	// set SlashDB username here
-const apiKey = "03zhx63tfaaj35zgesbikqg5uiqdla70";	// set SlashDB API key here
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
